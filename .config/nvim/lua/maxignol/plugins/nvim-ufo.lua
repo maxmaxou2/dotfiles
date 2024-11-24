1350772dd3ac2d21de02d287c7c39cf7ba620c77
Maxence Rossignol <maxence.rossignol@renewex.co>
2024-11-24 20:11:57 +0100

    add(folds using ufo)

---
+++ .config/nvim/lua/maxignol/plugins/nvim-ufo.lua
@@ -0,0 +1,62 @@
+local function fold_to_level(level)
+    local function inner()
+        return require("ufo").closeFoldsWith(level)
+    end
+    return inner
+end
+
+local handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+
+    local suffix = ("  %d lines"):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. ("."):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    local spaces = string.rep(".", 80 - curWidth - sufWidth)
+    suffix = " " .. spaces .. suffix
+    table.insert(newVirtText, { suffix, "Comment" })
+    return newVirtText
+end
+
+return {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+        local ufo = require("ufo")
+        -- folding
+        vim.o.foldcolumn = "0" -- '0' is not bad
+        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+        ufo.setup({
+            provider_selector = function(bufnr, filetype, buftype)
+                return { "treesitter", "indent" }
+            end,
+            fold_virt_text_handler = handler,
+        })
+        vim.keymap.set("n", "wR", ufo.openAllFolds)
+        vim.keymap.set("n", "<leader>w&", fold_to_level(0))
+        vim.keymap.set("n", "<leader>wé", fold_to_level(1))
+        vim.keymap.set("n", '<leader>w"', fold_to_level(2))
+        vim.keymap.set("n", "<leader>w'", fold_to_level(3))
+        vim.keymap.set("n", "<leader>w(", fold_to_level(4))
+    end,
+}
