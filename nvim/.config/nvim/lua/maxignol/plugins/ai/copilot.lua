535b80b1807ffbefdf8f779cdce94109eadfa3d4
Maxence Rossignol <maxence.rossignol@renewex.co>
2025-07-26 22:25:10 +0200

    add(copilot):now we have copilot support hehehehehe

--- nvim/.config/nvim/lua/maxignol/plugins/ai/copilot.lua
+++ nvim/.config/nvim/lua/maxignol/plugins/ai/copilot.lua
@@ -1,6 +1,24 @@
 return {
 	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
 	config = function()
-		require("copilot").setup({})
+		require("copilot").setup({
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				hide_during_completion = true,
+				debounce = 75,
+				trigger_on_accept = true,
+				keymap = {
+					accept = "<Tab>", -- Accept suggestion
+					accept_word = "<C-j>", -- Accept word
+					accept_line = "<C-l>", -- Accept line
+					next = "<C-n>", -- Next suggestion
+					prev = "<C-p>", -- Previous suggestion
+					dismiss = "<C-e>", -- Dismiss suggestion
+				},
+			},
+		})
 	end,
 }
