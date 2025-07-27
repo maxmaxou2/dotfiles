dc01458d0d047faedf1053cda2aee6a0f9859439
Maxence Rossignol <maxence.rossignol@renewex.co>
2025-07-27 22:01:37 +0200

    upd(maximizer):simplify logic but better working now

--- nvim/.config/nvim/lua/maxignol/plugins/maximizer.lua
+++ nvim/.config/nvim/lua/maxignol/plugins/maximizer.lua
@@ -5,25 +5,43 @@ local function get_tmux_window()
 	return vim.fn.trim(vim.fn.system({ "tmux", "display-message", "-p", "#I" }))
 end
 
+local function is_tmux_zoomed()
+	if not vim.env.TMUX then
+		return false
+	end
+	local status = vim.fn.system({ "tmux", "display-message", "-p", "#{window_zoomed_flag}" })
+	return vim.fn.trim(status) == "1"
+end
+
 return {
 	"szw/vim-maximizer",
 	lazy = false,
 	config = function()
 		local zoomed_win = nil
 		local zoomed_tmux_window = nil
-		vim.keymap.set("n", "<leader>wz", function()
-			local cur = vim.api.nvim_get_current_win()
+        local fake_zoom = false
+		local function toggle_nvim_zoom()
 			if zoomed_win then
 				zoomed_win = nil
 				zoomed_tmux_window = nil
 			else
-				zoomed_win = cur
+				zoomed_win = vim.api.nvim_get_current_win()
 				zoomed_tmux_window = get_tmux_window()
 			end
-			vim.cmd("MaximizerToggle!")
+			if #vim.api.nvim_tabpage_list_wins(0) <= 1 then
+                fake_zoom = true
+				return
+            elseif fake_zoom then
+                fake_zoom = false
+                return
+            end
+			vim.cmd("MaximizerToggle")
+		end
+		vim.keymap.set("n", "<leader>wz", function()
+			toggle_nvim_zoom()
 		end, { desc = "Toggle maximize split and record state" })
 
-        -- Handle focus events to manage zoom state
+		-- Handle focus events to manage zoom state
 		vim.api.nvim_create_autocmd("FocusLost", {
 			callback = function()
 				if not zoomed_win then
@@ -31,45 +49,20 @@ return {
 				end
 
 				local cur_win = get_tmux_window()
-				if zoomed_tmux_window and cur_win == zoomed_tmux_window then
-					vim.cmd("MaximizerToggle!")
-					zoomed_win = nil
-					zoomed_tmux_window = nil
+				if zoomed_tmux_window and cur_win == zoomed_tmux_window and not is_tmux_zoomed() then
+					toggle_nvim_zoom()
 				end
 			end,
 		})
 		vim.api.nvim_create_autocmd("WinEnter", {
 			callback = function()
-				if zoomed_win and vim.api.nvim_get_current_win() ~= zoomed_win then
-					zoomed_win = nil
-					zoomed_tmux_window = nil
-					if vim.env.TMUX then
+				if zoomed_win then
+					if vim.env.TMUX and is_tmux_zoomed() then
 						vim.fn.system({ "tmux", "resize-pane", "-Z" })
 					end
-					vim.cmd("MaximizerToggle!")
+					toggle_nvim_zoom()
 				end
 			end,
 		})
-
-        -- Handle case when split commands are used
-		local function unzoom_if_needed()
-			if zoomed_win then
-				zoomed_win = nil
-				vim.cmd("MaximizerToggle!")
-			end
-		end
-		vim.api.nvim_create_user_command("Split", function(opts)
-			unzoom_if_needed()
-			vim.cmd("split " .. opts.args)
-		end, { nargs = "*" })
-
-		vim.api.nvim_create_user_command("Vsplit", function(opts)
-			unzoom_if_needed()
-			vim.cmd("vsplit " .. opts.args)
-		end, { nargs = "*" })
-		vim.cmd([[
-          cabbrev <expr> split v:lua.require'zoom'.cmd_split()
-          cabbrev <expr> vsplit v:lua.require'zoom'.cmd_vsplit()
-        ]])
 	end,
 }
